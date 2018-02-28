8ee519f99cef3434248a4a1088ff8acf54205b58
Veronica Mihai <veraM@Veronica-Mihai.AD.UPPTU.BBU>
2018-02-28 16:08:23 +0200

    added control structures in blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,17 @@
 @section('content')
 <div class="row">
     <div class="row-md-12">
-        <h1>Some Content</h1>
-        <p>{{ 2 == 3 ? 'Hello':'Does not equal'}}</p>
+        <h1>Control Structures</h1>
+         
+         @if(false)
+            <p>This shows only if true</p>
+        @else
+            <p>This shows only if false</p>
+        @endif
+        <hr>
+        @for($i=0; $i < 5; $i++)
+            <p> {{$i + 1}}</p>
+        @endfor
     </div>
 </div>
 
