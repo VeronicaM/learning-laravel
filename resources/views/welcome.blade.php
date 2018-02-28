48f65f09b7912e98ae051cee34a3722aafb49a54
Veronica Mihai <veraM@Veronica-Mihai.AD.UPPTU.BBU>
2018-02-28 16:56:54 +0200

    added external css with URL::to(path_to_css) facade to ensure correct absolute path on every page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,8 @@
 @section('content')
 <div class="row">
     <div class="row-md-12">
-        <h1>Control Structures</h1>
-         
-         @if(false)
-            <p>This shows only if true</p>
-        @else
-            <p>This shows only if false</p>
-        @endif
-        <hr>
-        @for($i=0; $i < 5; $i++)
-            <p> {{$i + 1}}</p>
-        @endfor
-        <p>XSS</p>
-        {{"<script>alert('Hello');</script>"}}
-        {!!"<script>alert('Hello');</script>"!!}
-    </div>
+       <p class="blog">A blog post here</p>
+</div>
 </div>
 
 @endsection
